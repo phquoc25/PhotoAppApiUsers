d9e7760b9bdc8762a0469d56a44f0e7d522b4b66
Quoc PHAN <phquoc25@gmail.com>
2021-01-03 17:36:08 +0700

    Allow only gateway ip address to access users apis

--- src/main/java/com/qph/springmicorservices/photoapp/api/users/security/WebSecurity.java
+++ src/main/java/com/qph/springmicorservices/photoapp/api/users/security/WebSecurity.java
@@ -1,16 +1,20 @@
 package com.qph.springmicorservices.photoapp.api.users.security;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+    private final Environment env;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/users/**").hasIpAddress(env.getProperty("gateway.ip"));
     }
 }
